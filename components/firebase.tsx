f21da6e03137a5e8d06ae3d1d9f38dc40b2431e7
User <giddtkipscheruyot@gmail.com>
2025-09-19 13:36:52 +0300

    am cooked

--- components/firebase.tsx
+++ components/firebase.tsx
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 export const firebaseConfig = {
-  apiKey: "AIzaSyDzYHXQr3aUxAwKgUooKbT66wjdTeXcxA4",
-  authDomain: "singup-da798.firebaseapp.com",
-  databaseURL: "https://singup-da798-default-rtdb.firebaseio.com",
-  projectId: "singup-da798",
-  storageBucket: "singup-da798.firebasestorage.app",
-  messagingSenderId: "200805347343",
-  appId: "1:200805347343:web:adc340fa569a8cdfc6f37a",
-  measurementId: "G-ZSXC4LGMZC"
+  apiKey: ,
+  authDomain: ,
+  databaseURL: ,
+  projectId: ,
+  storageBucket: ,
+  messagingSenderId:,
+  appId: ,
 };
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
